87acf973c4660e9ef91d3888ed5e29faf6a7b363
Norbert Nemec <norbert@nemec-online.de>
2017-01-05 22:53:12 +0100

    ImapClient: AccountData stored within imapcache, enable open cache without logging in

--- app/imapclient.service.ts
+++ app/imapclient.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 
 import { PouchCache } from './pouch-cache';
 
 declare const ImapClient: any;
-declare const ElectronStorage: any;
 
 export class AccountData {
   host: string = "";
@@ -13,26 +12,38 @@ export class AccountData {
 }
 
 @Injectable()
-export class ImapClientService {
-  accountData: AccountData;
+export class ImapClientService implements OnInit {
+  cache = new PouchCache;
+  accountData = new AccountData;
   imapClient: any;
-  mailboxes: any;
-  cache: PouchCache;
-  selectedPath: string;
+  mailboxes: any = { root: true, children: [] };
 
-  constructor() {
-    this.accountData = new AccountData;
-    ElectronStorage.get('AccountData').then(data=>{
-      this.accountData = data;
-    });
-    this.cache = new PouchCache();
+  isOpen(): boolean {
+    return this.cache.isOpen();
+  }
+
+  ngOnInit() {
+    return this.cache.open("imapcache").catch((err)=>{
+      throw err;
+    }).then(()=>{
+      this.cache.retrieve("account").then((doc) => {
+        this.accountData = doc;
+      });
+      this.cache.observe("mailboxes").subscribe((doc)=>{
+        this.mailboxes = doc;
+      });
+    })
+  }
+
+  close(): void {
+    this.cache.close();
   }
 
   isLoggedIn(): boolean {
     return this.imapClient && this.mailboxes && this.cache.isOpen();
   }
 
-  login(): void {
+  login(): Promise<any> {
     this.imapClient = new ImapClient(
       this.accountData.host,
       this.accountData.port,
@@ -46,23 +57,13 @@ export class ImapClientService {
 
     this.imapClient.logLevel = ImapClient.LOG_LEVEL_INFO;
 
-    this.imapClient.connect().catch((err)=>{
+    return this.imapClient.connect().catch((err)=>{
       console.error("login failed");
       this.logout();
       throw err;
     }).then(()=>{
       console.info("logged in");
-      ElectronStorage.set('AccountData', this.accountData);
-      let cacheName = "imapcache:"+this.accountData.user+"@"+this.accountData.host+":"+this.accountData.port;
-      this.cache.open(cacheName).catch((err)=>{
-        this.logout();
-        throw err;
-      }).then(()=>{
-        this.cache.observe("mailboxes").subscribe((doc)=>{
-          this.mailboxes = doc;
-        });
-        this.updateMailboxes();
-      });
+      this.cache.store("account",this.accountData);
     });
   }
 
@@ -71,46 +72,27 @@ export class ImapClientService {
       this.imapClient.close();
       this.imapClient = undefined;    
     }
-    this.cache.close();
     this.mailboxes = undefined;
-    this.selectedPath = undefined;
   }
 
-  updateMailboxes(): void {
-    this.selectedPath = undefined;
-    this.imapClient.listMailboxes().then((mailboxes)=>{
+  updateMailboxes(): Promise<any> {
+    return this.imapClient.listMailboxes().then((mailboxes)=>{
       return this.cache.store("mailboxes", mailboxes);
-    }).then(()=>{
-/*
-      let updateRecursively = ((children:any[])=>{
-        for(let child of children) {
-          this.select(child.path);
-          updateRecursively(child.children);
-        }
-      })
-      updateRecursively(this.mailboxes.children)
-*/
     });
   }
 
-  select(path: string): void {
-    this.selectedPath = path;
+  updateMailbox(path: string): void {
     this.imapClient.selectMailbox(path,{readOnly:true}).then((mailbox)=>{
       return this.cache.store("mailbox:"+path, mailbox);
     }).then(()=>{
       let p_imapmsgs = this.imapClient.listMessages(path,"1:*",['uid']);
-      let p_cachemsgs = this.cache.query_ids_by_prefix("message:"+path+":");
+      let p_cachemsgs = this.cache.query_ids_by_prefix("envelope:"+path+":");
       return Promise.all([p_imapmsgs,p_cachemsgs]);
     }).then(([imapmsgs,cachemsgs])=>{
       let imapuids = new Set(imapmsgs.map(({uid})=>uid));
       let cacheuids = new Set(cachemsgs.rows.map(({id})=>Number(id.split(':').pop())));
       let to_delete = Array.from(cacheuids).filter(uid=>!imapuids.has(uid));
       let to_create = Array.from(imapuids).filter(uid=>!cacheuids.has(uid));
-
-      // console.log(imapuids);
-      // console.log(cacheuids);
-      // console.log(to_delete);
-      // console.log(to_create);
 
       // handle to_delete here
 
@@ -120,16 +102,28 @@ export class ImapClientService {
     }).then((new_messages)=>{
       for(let message of new_messages) {
         delete message["#"];
-        message._id = "message:"+path+":"+message.uid;
+        message._id = "envelope:"+path+":"+message.uid;
       }
 
       // console.log("storing messages: " + JSON.stringify(new_messages,null,'\t'));
 
       return this.cache.db.bulkDocs(new_messages);
     }).then(()=>{
-      return this.cache.query_ids_by_prefix("message:"+path+":");
+      return this.cache.query_ids_by_prefix("envelope:"+path+":");
     }).then((messages)=>{
       // console.log("retrieved messages: " + JSON.stringify(messages,null,'\t'));
+    });
+  }
+
+  updateAll(): void {
+    this.updateMailboxes().then(()=>{
+      let updateRecursively = ((children:any[])=>{
+        for(let child of children) {
+          this.updateMailbox(child.path);
+          updateRecursively(child.children);
+        }
+      })
+      updateRecursively(this.mailboxes.children);
     });
   }
 }
