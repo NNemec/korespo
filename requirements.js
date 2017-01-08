59639a0cce655ffa54a214b5b37b57eb7bdf83fd
Norbert Nemec <norbert@nemec-online.de>
2017-01-08 15:57:48 +0100

    remove unused dependency

--- requirements.js
+++ requirements.js
@@ -2,7 +2,6 @@
 // but it is the only way I could get it work...
 
 const ImapClient = require('emailjs-imap-client');
-const ElectronStorage = require('electron-storage');
 const NodePouchDB = require('pouchdb');
 const deepEqual = require('deep-equal');
 
