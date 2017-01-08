68d85392b55f08af80c34c4b8102daf12e9900b1
Norbert Nemec <norbert@nemec-online.de>
2017-01-08 12:57:28 +0100

    suppress warning for too many EventEmitter listeners

--- requirements.js
+++ requirements.js
@@ -7,3 +7,8 @@ const NodePouchDB = require('pouchdb');
 const deepEqual = require('deep-equal');
 
 const ElectronRemote = require('electron').remote;
+
+// workaround: suppress warning for too many listeners on inaccessible levelChanges EventEmitter
+// for discussion, see https://github.com/pouchdb/pouchdb/issues/6123
+const EventEmitter = require('events');
+EventEmitter.defaultMaxListeners = 0;
