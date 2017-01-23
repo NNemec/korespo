750c35da751e9ad4f188868466e97fbfdc85891e
Norbert Nemec <norbert@nemec-online.de>
2017-01-23 08:32:25 +0100

    add VS building&launching mechanism for debug.ts file

---
+++ debug.ts
@@ -0,0 +1,12 @@
+console.log("something")
+
+import { ImapClient } from './lib/imapclient';
+
+let imapClient = new ImapClient('debugdb');
+
+imapClient.dbAccess().info().then((info)=>{
+    console.log(info);
+}).catch((err)=>{
+    console.error(err);
+})
+
