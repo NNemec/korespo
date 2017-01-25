af0391b248e9b3296f211f9a7ada6d6223a7b298
Norbert Nemec <norbert@nemec-online.de>
2017-01-25 19:02:16 +0100

    small improvements to ImapModel interfaces

--- lib/imapmodel.ts
+++ lib/imapmodel.ts
@@ -6,36 +6,40 @@ interface Address {
   readonly name: string;
   readonly addr: string;
 
-  readonly countFilteredMessages: Observable<Number>;
-  readonly countFilteredMessagesTo: Observable<Number>;
-  readonly countFilteredMessagesFrom: Observable<Number>;
+  readonly countFilteredMessages: Observable<Number>;     // ignoring Address filter
+  readonly countFilteredMessagesFrom: Observable<Number>; // ignoring Address filter
+  readonly countFilteredMessagesTo: Observable<Number>;   // ignoring Address filter
+
+  filterSelectedFrom: boolean;
+  filterSelectedTo: boolean;
 };
 
 interface Folder {
   name: string;
   children: Folder[];
 
-  readonly countFilteredMessages: Observable<Number>;
-}
+  readonly countFilteredMessages: Observable<Number>;     // ignoring Folder filter
+
+  filterSelected: boolean;
+};
 
 interface Message {
   readonly subject: string;
   readonly from: Address[];
   readonly to: Address[];
   readonly date: Moment;
-}
+
+  readonly folder: Folder;
+};
 
 interface ImapModel {
   readonly folders: Observable<Folder>;
   readonly addresses: Observable<Address[]>;
 
-  filterAddresses: Address[];
-  filterFolders: Folder[];
-
-  readonly filteredDates: Observable<Moment[]>; // ignoring filtered(Start|End)Date
+  readonly filteredDates: Observable<Moment[]>;           // ignoring Date filter
 
   filterStartDate: Moment;
   filterEndDate: Moment;
 
   readonly filteredMessages: Observable<Message[]>;
-}
+};
